e81fd5475663091d8362c5ea064ab8ab1b9debdb
Pablo Gerardo González López <pablog@ciencias.unam.mx>
2022-01-22 16:13:16 -0600

    Add coeffects to generate random elements

--- src/matrix_effect/events.cljs
+++ src/matrix_effect/events.cljs
@@ -3,6 +3,17 @@
    [matrix-effect.db :as db]
    [re-frame.core :as re-frame]))
 
+(re-frame/reg-cofx
+ ::random-numbers
+ (fn [cofx n]
+   (assoc cofx :rand-nums (repeatedly n rand))))
+
+(re-frame/reg-cofx
+ ::random-chars
+ (fn [cofx n]
+   (let [random-char (fn [] (String/fromCharCode (* (rand) 128)))]
+     (assoc cofx :rand-chars (repeatedly n random-char)))))
+
 (re-frame/reg-event-db
  ::initialize-db
  (fn [_ _]
