52a7d2ce48707981b8e5010455452f26f5357255
Pablo Gerardo González López <pablog@ciencias.unam.mx>
2022-01-22 16:48:59 -0600

    Add event that calculates the next frame

--- src/matrix_effect/events.cljs
+++ src/matrix_effect/events.cljs
@@ -15,6 +15,39 @@
    (let [random-char (fn [] (String/fromCharCode (* (rand) 128)))]
      (assoc cofx :rand-chars (repeatedly n random-char)))))
 
+
+(re-frame/reg-fx
+ ::draw-frame
+ (fn [[canvas y-pos chars]]
+   (canvas/draw-rectangle canvas "#0001" 1000 10000)
+   (run!
+     (fn [[idx y]]
+       (let [x (* idx 20)]
+         (canvas/draw-character canvas (nth chars idx) "#0F0" x y)))
+     (map-indexed vector y-pos))))
+
+(defn next-y-pos
+  "Takes a `y-pos` seq and generates `y`s values (moving them 20px down) for
+   the next frame. According to `rand-nums`, randomly resets the end of the
+   column if it's at least 100px high."
+  [y-pos rand-nums]
+  (->> y-pos
+       (map-indexed vector)
+       (map (fn [[idx y]]
+              (if (> y (+ 100 (* (nth rand-nums idx) 10000)))
+                0
+                (+ y 20))))))
+
+(re-frame/reg-event-fx
+ ::next-frame
+ [(re-frame/inject-cofx ::canvas/get-canvas)
+  (re-frame/inject-cofx ::random-numbers 51)
+  (re-frame/inject-cofx ::random-chars 51)]
+ (fn [{:keys [db] :as cofx} _]
+   {:db          (update db :y-pos next-y-pos (:rand-nums cofx))
+    ::draw-frame [(:canvas cofx) (:y-pos db) (:rand-chars cofx)]}))
+
+
 (re-frame/reg-event-fx
  ::initialize
  [(re-frame/inject-cofx ::canvas/get-canvas)]
--- src/matrix_effect/views.cljs
+++ src/matrix_effect/views.cljs
@@ -1,12 +1,16 @@
 (ns matrix-effect.views
   (:require
+   [matrix-effect.events :as events]
    [matrix-effect.subs :as subs]
    [re-frame.core :as re-frame]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "The matrix-effect"]
+     [:div
+      [:h1 "The matrix-effect"]
+      [:button {:on-click #(re-frame/dispatch [::events/next-frame])}
+       "Next frame"]]
      [:canvas {:id     "canv"
                :width  1000
                :height 10000}]]))
